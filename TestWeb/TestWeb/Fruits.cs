e1abfc6a75c7632ecb48be2715b21ea6a1b8169a
nam33 <namra3@yahoo.com>
2016-12-19 17:24:42 -0500

    Add files via upload

---
+++ TestWeb/TestWeb/Fruits.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestWeb
+{
+    public class Fruits
+    {
+        public double  appPrice = 0.45;
+        public double  orgPrice = 0.55;
+
+        public string fruitName;
+        public int fruitQty;
+
+       public  double calculatePrice(List<Fruits> fr)
+        {
+            int appQty=0;
+            int orgQty=0;
+            double appleFinalPrice = 0;
+           double orangeFinalPrice = 0;
+           
+
+
+           for(int i= 0; i<fr.Count; i++)
+           {
+               if (fr[i].fruitName.Equals("apple"))
+                   appQty = fr[i].fruitQty;                   
+               if (fr[i].fruitName.Equals("orange"))
+                   orgQty = fr[i].fruitQty;
+           }
+
+           appleFinalPrice = calculatePromoPriceItem(appQty, 2, 1) * appPrice;
+           orangeFinalPrice = calculatePromoPriceItem(orgQty, 3, 2) * orgPrice;
+
+          
+           return (appleFinalPrice + orangeFinalPrice);
+        }
+
+
+       public int calculatePromoPriceItem(int qty, int reqdMultiItem, int applyMultiItem)
+       {
+           int retQty = qty; 
+           retQty=   ( ((int)(qty/reqdMultiItem)) *applyMultiItem) + (int)(qty%reqdMultiItem);
+           return retQty;
+       }
+
+    }
+}
---
+++ TestWeb/TestWeb/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestWeb
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Fruits> myFruit = new List<Fruits>();
+
+            Console.WriteLine("enter qty for apples");
+            string app = Console.ReadLine();
+
+            Fruits fapple = new Fruits();
+
+            fapple.fruitName = "apple";
+            fapple.fruitQty = Convert.ToInt32(app);
+            
+            Console.WriteLine("enter qty for oranges");
+            string org = Console.ReadLine();
+
+            Fruits forange = new Fruits();
+
+            forange.fruitName = "orange";
+            forange.fruitQty = Convert.ToInt32(org);
+
+
+            myFruit.Add(fapple);
+            myFruit.Add(forange);
+
+            Fruits f = new Fruits();
+
+            Console.WriteLine("Total price is " + f.calculatePrice(myFruit));
+        }
+    }
+}
